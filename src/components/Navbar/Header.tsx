4d23c302f33649500f593e57162709eeeb1dc940
Hr_28215 <harshalborad2003@gmail.com>
2025-02-15 14:51:03 +0530

    feat: add header and sidebar components with avatar functionality

--- src/components/Navbar/Header.tsx
+++ src/components/Navbar/Header.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useRef } from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
 
 function Header() {
-  return (
-    <header>
-      
-    </header>
-  )
+    const [isOpen, setIsOpen] = React.useState(false)
+    const sidebarRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)) {
+                setIsOpen(false)
+            }
+        }
+
+        if (isOpen) {
+            document.addEventListener("mousedown", handleClickOutside)
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+    }, [isOpen])
+
+    return (
+        <header className='relative'>
+            <Navbar setIsOpen={setIsOpen} isOpen={isOpen} />
+            <div ref={sidebarRef}>
+                <Sidebar isOpen={isOpen} />
+            </div>
+        </header>
+    )
 }
 
 export default Header
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,16 @@
+import { SidebarClose, SidebarOpen } from 'lucide-react'
 import React from 'react'
+import { AvatarDemo } from './Avtar'
 
-function Navbar() {
+function Navbar({ setIsOpen, isOpen }: { setIsOpen: React.Dispatch<React.SetStateAction<boolean>>, isOpen: boolean }) {
   return (
-    <nav>
-      
+    <nav className='w-[90%] rounded-full bg-white/5 backdrop-blur-xl shadow-md p-4 mx-auto mt-4 flex justify-between items-center'>
+      {isOpen ?
+        <SidebarClose onClick={() => setIsOpen(false)} className='text-white hover:cursor-pointer' />
+        :
+        <SidebarOpen onClick={() => setIsOpen(true)} className='text-white hover:cursor-pointer' />
+      }
+      <AvatarDemo />
     </nav>
   )
 }
